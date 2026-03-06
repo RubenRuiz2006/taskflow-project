c98f6439710c8f7fb5230e058b94455886f7f737
Rubén Ruiz Mayorga <rubenruizmay18@gmail.com>
2026-03-06 08:45:20 +0100

    implementación de conectividad

--- code.js
+++ code.js
@@ -5,23 +5,22 @@ const btnConfirmar = document.getElementById("botonConfirmar")
 const section = document.getElementById("section")
 let tareas=[];
 
-const tareasGuardadas=localStorage.getItem(tareas);
-const tareasConservadas= JSON.parse(tareasGuardadas) //aqui nos quedamos por hoy para implementar el localStorage y hay que hacer que cuando elimine alguna tarea se elimine del array tareas tambien
+const tareasGuardadas=localStorage.getItem("tareas"); //miramos si existen tareas guardadas en localStorage
+if(tareasGuardadas){
+    tareas = JSON.parse(tareasGuardadas)
+     tareas.forEach(t => {
+        crearTarea(t.nombre, t.categoría, t.prioridad)
+    })
+} 
 
 añadir.addEventListener("click", () =>{ //boton para ir a la ventana de añadir tarea
 ventanaAñadir.style.display="flex";
 contenedor.style.display="none"
-
-
 })
 
-btnConfirmar.addEventListener("click", () =>{
-    const nombre=document.getElementById("nombre").value
-    const categoría =document.getElementById("categoría").value
-    const prioridad=document.getElementById("prioridad").value
 
-
-if(nombre!="" && categoría!=""){ //me aseguro de que introduzcan esos datos
+function crearTarea(nombre, categoría, prioridad){ //función para solo crear la tarea en el DOM
+    
 const nuevaTarea = document.createElement("div");
 nuevaTarea.className="tarea";
 nuevaTarea.innerHTML=`
@@ -34,10 +33,12 @@ nuevaTarea.innerHTML=`
 section.appendChild(nuevaTarea)
 
 
-const btnQuitar = nuevaTarea.querySelector(".buttonQuitar")
+const btnQuitar = nuevaTarea.querySelector(".buttonQuitar")//dentro le añadimos el boton para eliminarlo
 
-btnQuitar.addEventListener("click", () => {
+btnQuitar.addEventListener("click", () => { //boton para borrar cada tarea del DOM y del array
     nuevaTarea.remove()
+   tareas = tareas.filter(t => t.nombre!=nombre)
+   localStorage.setItem("tareas", JSON.stringify(tareas))
 })
 
 
@@ -47,18 +48,28 @@ document.getElementById("categoría").value=""
 
 ventanaAñadir.style.display="none"
 contenedor.style.display="flex"
-
-const tarea={ //aquí añadimos esta nueva tarea al array para guardarlo posteriormente en localstorage
-    nombre: nombre,
-    categoría: categoría,
-    prioridad: prioridad};
-
-tareas.push(tarea);
-localStorage.setItem("tareas", JSON.stringify(tareas))
-
-}else{
-    alert("Debe rellenar cada campo")
 }
 
-})
 
+
+btnConfirmar.addEventListener("click", () => { //para enviar el formulario
+    const nombre = document.getElementById("nombre").value
+    const categoría = document.getElementById("categoría").value
+    const prioridad = document.getElementById("prioridad").value
+
+    if (nombre != "" && categoría != "") { //me aseguro de que introduzcan esos datos
+
+        const tarea = { //aquí añadimos esta nueva tarea al array para guardarlo posteriormente en localstorage
+            nombre: nombre,
+            categoría: categoría,
+            prioridad: prioridad
+        };
+
+        tareas.push(tarea);
+        localStorage.setItem("tareas", JSON.stringify(tareas))
+        crearTarea(nombre, categoría, prioridad)
+
+    } else {
+        alert("Debe rellenar cada campo")
+    }
+})
